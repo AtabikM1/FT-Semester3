a2b02a199b2da439aad4e191783a6e504b998867
Pramudya Surya Anggara Putra <prasuatra@gmail.com>
2024-10-24 07:04:11 +0700

    Adding logo under hero's landing page

--- Coterra-next/connecttopia/src/app/page.tsx
+++ Coterra-next/connecttopia/src/app/page.tsx
@@ -9,13 +9,16 @@ import Benefits from "./components/index/features";
 import PlatformPerformanceChart from "./components/index/charts";
 import IndexCTA from "./components/index/join-now";
 import IndexIntro from "./components/index/intro";
+import IndexLogo from "./components/index/logo";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <IndexHero /> 
-      <IndexIntro/>{/*
+      <IndexHero />
+      <IndexLogo />
+      <IndexIntro />
+      {/*
       <IndexIntroduction />
       <Benefits />
       <PlatformPerformanceChart />
